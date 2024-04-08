783882c65913a81fef8ee872b63931a5b950ecf1
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2024-04-08 12:55:04 +0200

    Readd resources
    also remove dependencies as they are no longer required

---
+++ src/client/java/urlmusicdiscs/URLDiscItem.java
@@ -0,0 +1,10 @@
+package urlmusicdiscs;
+
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.sound.SoundEvent;
+
+public class URLDiscItem extends MusicDiscItem {
+    public URLDiscItem(int comparatorOutput, SoundEvent sound, Settings settings, int lengthInSeconds) {
+        super(comparatorOutput, sound, settings, lengthInSeconds);
+    }
+}
--- src/client/java/urlmusicdiscs/URLMusicDiscsClient.java
+++ src/client/java/urlmusicdiscs/URLMusicDiscsClient.java
@@ -2,12 +2,31 @@ package urlmusicdiscs;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 public class URLMusicDiscsClient implements ClientModInitializer {
     public static final String MOD_ID = "urlmusicdiscs";
     public static final Identifier CUSTOM_RECORD_PACKET_ID = new Identifier(MOD_ID, "play_sound");
     public static final Identifier CUSTOM_RECORD_GUI = new Identifier(MOD_ID, "record_gui");
+    public static final Identifier CUSTOM_RECORD_SET_URL = new Identifier(MOD_ID, "record_set_url");
+    public static final Identifier PLACEHOLDER_SOUND_IDENTIFIER = new Identifier(MOD_ID, "placeholder_sound");
+    public static final SoundEvent PLACEHOLDER_SOUND = Registry.register(
+            Registries.SOUND_EVENT,
+            PLACEHOLDER_SOUND_IDENTIFIER,
+            SoundEvent.of(PLACEHOLDER_SOUND_IDENTIFIER)
+    );
+    public static final Item CUSTOM_RECORD = Registry.register(
+            Registries.ITEM,
+            new Identifier(MOD_ID, "custom_record"),
+            new URLDiscItem(17, null, new FabricItemSettings().maxCount(1), 1)
+    );
 
     @Override
     public void onInitializeClient() {
@@ -15,9 +34,14 @@ public class URLMusicDiscsClient implements ClientModInitializer {
             client.execute(() -> {
             });
         });
+
         ClientPlayNetworking.registerGlobalReceiver(CUSTOM_RECORD_GUI, (client, handler, buf, responseSender) -> {
             client.execute(() -> {
             });
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register((content) -> {
+            content.add(CUSTOM_RECORD);
         });
     }
 }
